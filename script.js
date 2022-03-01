da1a0af5c2a1478574e23abc67dd88ea7e784c18
“yornvickie” <yornvickie@gmail.com>
2022-03-01 14:38:16 -0600

    added Variables Exercise Folder

---
+++ script.js
@@ -0,0 +1,18 @@
+// const
+
+let num = 20;
+num = 15;
+num = num + 7;
+num--;
+num = "This is not a number";
+let blank;
+console.log(blank);
+blank = null;
+
+// BONUS
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+console.log(quarter, dime, nickel, penny);
+
+let letters = "qwertyuiop";
+console.log(letters.length);
+console.log(letters[9]);
